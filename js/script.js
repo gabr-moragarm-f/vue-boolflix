327953c85a7071add6a663c61c0573d754da32d6
Gabriel Moraga <agarom97@gmail.com>
2021-02-11 11:28:37 +0100

    milestone 2 - 3

--- js/script.js
+++ js/script.js
@@ -2,15 +2,33 @@ var app = new Vue({
   el: '#root',
   data:{
     userSearchInput: '',
-    searchResult: []
+    searchResultMovie: [],
+    searchResultShows: []
   },
   methods:{
-    searchMovie: function() {
+    searchMoviesAndShows: function() {
+      this.searchMovies();
+
+      this.searchShows();
+    },
+    searchMovies:function() {
       let self = this;
+
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=8ef7b50081ce77b4e68be605e52ac6b2&query=' + encodeURI(this.userSearchInput))
       .then(function(response) {
-        self.searchResult = response.data.results
+        self.searchResultMovie = response.data.results
       });
+    },
+    searchShows:function() {
+      let self = this;
+
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=8ef7b50081ce77b4e68be605e52ac6b2&query=' + encodeURI(this.userSearchInput))
+      .then(function(response) {
+        self.searchResultShows = response.data.results
+      });
+    },
+    voteToStars: function(vote) {
+      return Math.ceil(vote / 2);
     }
   }
 });
